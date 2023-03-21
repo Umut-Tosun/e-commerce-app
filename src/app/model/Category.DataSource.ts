54fb6916fa89b747f252df284966186aa83296fb
umut tosun <youremail@yourdomain.com>
2023-03-21 15:53:19 +0300

    New column for category.

--- src/app/model/Category.DataSource.ts
+++ src/app/model/Category.DataSource.ts
@@ -2,6 +2,11 @@ import { Category } from "./Category";
 import { Product } from "./Product";
 
 export const CategoryList:Category[]=[
-    {Id:1,Name:"Teknoloji",ImagePath:"Test.png",Description:"Test Açıklaması",ProductListWithByCategory:[]}
+    {Id:1,Name:"Teknoloji",ImagePath:"Test.png",Description:"Test Açıklaması",ProductListWithByCategory:[],Color:"primary"},
+    {Id:2,Name:"Beyaz Eşya",ImagePath:"Test.png",Description:"Test Açıklaması",ProductListWithByCategory:[],Color:"warning"},
+    {Id:3,Name:"Hediyelik Eşya",ImagePath:"Test.png",Description:"Test Açıklaması",ProductListWithByCategory:[],Color:"info"},
+    {Id:4,Name:"Petshop",ImagePath:"Test.png",Description:"Test Açıklaması",ProductListWithByCategory:[],Color:"success"},
+    {Id:5,Name:"Giyim",ImagePath:"Test.png",Description:"Test Açıklaması",ProductListWithByCategory:[],Color:"danger"},
+    {Id:6,Name:"İnşaat",ImagePath:"Test.png",Description:"Test Açıklaması",ProductListWithByCategory:[],Color:"dark"},
     
 ]
