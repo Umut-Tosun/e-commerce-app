cabe6e97cf14b9791952d88579533276c55e6646
umuttosun23 <youremail@yourdomain.com>
2023-03-21 11:54:18 +0300

    Add new data.

--- src/app/model/Product.DataSource.ts
+++ src/app/model/Product.DataSource.ts
@@ -2,6 +2,16 @@ import { CategoryList } from "./Category.DataSource";
 import { Product } from "./Product";
 
 export const ProductList:Product[]=[
-    {Id:1,Category:CategoryList[0],Name:"Test",ImagePath:"Test.png",UnitPrice:455,Stock:200,Status:true}
+    {Id:1,Category:CategoryList[0],Name:"Test",ImagePath:"Test.png",UnitPrice:455,Stock:200,Status:true},
+    {Id:1,Category:CategoryList[0],Name:"Test",ImagePath:"Test.png",UnitPrice:455,Stock:200,Status:true},
+    {Id:1,Category:CategoryList[0],Name:"Test",ImagePath:"Test.png",UnitPrice:455,Stock:200,Status:true},
+    {Id:1,Category:CategoryList[0],Name:"Test",ImagePath:"Test.png",UnitPrice:455,Stock:200,Status:true},
+    {Id:1,Category:CategoryList[0],Name:"Test",ImagePath:"Test.png",UnitPrice:455,Stock:200,Status:true},
+    {Id:1,Category:CategoryList[0],Name:"Test",ImagePath:"Test.png",UnitPrice:455,Stock:200,Status:true},
+    {Id:1,Category:CategoryList[0],Name:"Test",ImagePath:"Test.png",UnitPrice:455,Stock:200,Status:true},
+    {Id:1,Category:CategoryList[0],Name:"Test",ImagePath:"Test.png",UnitPrice:455,Stock:200,Status:true},
+    {Id:1,Category:CategoryList[0],Name:"Test",ImagePath:"Test.png",UnitPrice:455,Stock:200,Status:true},
+    {Id:1,Category:CategoryList[0],Name:"Test",ImagePath:"Test.png",UnitPrice:455,Stock:200,Status:true},
+    {Id:1,Category:CategoryList[0],Name:"Test",ImagePath:"Test.png",UnitPrice:455,Stock:200,Status:true},
 ]
     
