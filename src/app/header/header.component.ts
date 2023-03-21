5370163ec64cdbe35a4abeccba058198e3d6b9c7
tosun <tosun@LAPTOP-ETHQG58O>
2023-03-21 21:18:00 +0300

    Some changes navbar card li.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CartList } from '../model/Cart.DataSource';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  CartLength(){
+    return CartList.filter(x=>x.Status==true).length;
+  }
 }
