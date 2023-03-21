0fd87cb76c2a6c7600371ce7492bbc60c4c69a51
umuttosun23 <youremail@yourdomain.com>
2023-03-21 11:55:21 +0300

    Edit navbar.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:ProductsComponent},
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+
+export class AppRoutingModule {
+  
+ }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { Product } from '../model/Product';
+import {ProductsService} from '../products.service'
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
+
 export class ProductsComponent {
+  productList:Product[]=[];
+  constructor(private productService:ProductsService){}
+
+  ngOnInit():void{
+    this.getProductsFromService();
+  }
+
+   getProductsFromService():void{
+     this.productService.getProductList()
+     .subscribe(product=>{
+        this.productList=product;
+     });
+   }
 
 }
